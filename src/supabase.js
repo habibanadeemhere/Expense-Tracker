d9d132e6f46c990cfe61d9d25875455191e1c76f
25rabia <157057731+25rabia@users.noreply.github.com>
2026-04-03 15:25:24 +0500

    .

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-VITE_SUPABASE_URL="https://fbnnmhjmbnkaoaydblru.supabase.co"
-VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY= "sb_publishable_zP-j51sga24BZeD_bkP2Nw_IGf6Ktfy"
+const VITE_SUPABASE_URL="https://fbnnmhjmbnkaoaydblru.supabase.co"
+const VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY= "sb_publishable_zP-j51sga24BZeD_bkP2Nw_IGf6Ktfy"
 
 export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
